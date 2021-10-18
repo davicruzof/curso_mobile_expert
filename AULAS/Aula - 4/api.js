11e71a3c9dbe0077c955058f8810dd13b54ba809
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-10-17 21:17:35 -0300

    Delete api.js

--- AULAS/Aula - 4/api.js
+++
@@ -1,9 +0,0 @@
-export const key = "b4b7c6a8";
-
-const getApi = () => {
-    fetch(`https://api.hgbrasil.com/finance?key=${key}`, { method: 'GET'})
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-    })
-  }
